9b09e78f39a759c7f9c0013aae44d737825110d2
Linanxxu <458088496@qq.com>
2022-02-06 19:56:31 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author SAECOHU
+ * @Date 6/2/2022 下午 7:46
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
